8571ee16c81f1b4b7126f099a304009df9346c0b
cintia martinez <92597426+cincinati94@users.noreply.github.com>
2025-10-31 03:55:14 +0100

    desarrollo prueba

--- src/app/user/services/github.service.ts
+++ src/app/user/services/github.service.ts
@@ -2,28 +2,18 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { GithubRepo, GithubUser } from '../../models/github-user.model';
-import { environment } from '../../../environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class GithubService {
-
   private urlBase = 'https://api.github.com/users/';
-  token = environment.githubToken; 
 
-  headers = new HttpHeaders({
-      Authorization: `token ${this.token}`,
-    });
-
-  constructor(private httpClient:HttpClient){}
-    getListDataUser(name:String):Observable<GithubUser>{
-      this.headers
-      return this.httpClient.get<GithubUser>(`${this.urlBase}${name}`);
-    }
-    getListReposUser(name:String):Observable<GithubRepo[]>{
-      this.headers
-      return this.httpClient.get<GithubRepo[]>(`${this.urlBase}${name}/repos`);
-    }
-      
+  constructor(private httpClient: HttpClient) {}
+  getListDataUser(name: String): Observable<GithubUser> {
+    return this.httpClient.get<GithubUser>(`${this.urlBase}${name}`);
+  }
+  getListReposUser(name: String): Observable<GithubRepo[]> {
+    return this.httpClient.get<GithubRepo[]>(`${this.urlBase}${name}/repos`);
+  }
 }
--- src/environments/environment.ts
+++
@@ -1,3 +0,0 @@
-export const environment = {
-  githubToken: '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s'
-}
